381c4cd48b5a1937864f4ee52f34d8dc54385a39
Ilya Fomichev <fomil.music@gmail.com>
2021-08-08 03:41:14 +0700

    delete getSynchronousResult from InstallPermissionContract

--- app/src/main/java/solru/okkeipatcher/pm/activityresult/InstallPermissionContract.kt
+++ app/src/main/java/solru/okkeipatcher/pm/activityresult/InstallPermissionContract.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
+import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.annotation.RequiresApi
 import solru.okkeipatcher.MainApplication
@@ -13,13 +14,10 @@ import solru.okkeipatcher.pm.PackageManager
 class InstallPermissionContract : ActivityResultContract<Unit, Boolean>() {
 
 	override fun createIntent(context: Context, input: Unit?) = Intent(
-		android.provider.Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES,
+		Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES,
 		Uri.parse("package:${PackageManager.packageName}")
 	)
 
 	override fun parseResult(resultCode: Int, intent: Intent?) =
 		MainApplication.context.packageManager.canRequestPackageInstalls()
-
-	override fun getSynchronousResult(context: Context, input: Unit?) =
-		SynchronousResult(MainApplication.context.packageManager.canRequestPackageInstalls())
 }
