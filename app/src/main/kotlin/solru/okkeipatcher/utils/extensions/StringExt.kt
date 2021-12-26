be6f2673dc6a21994ac43f0a29f57276971cb11a
Ilya Fomichev <fomil.music@gmail.com>
2021-12-26 09:40:09 +0700

    delete DebugUtil

--- app/src/main/kotlin/solru/okkeipatcher/utils/extensions/StringExt.kt
+++ app/src/main/kotlin/solru/okkeipatcher/utils/extensions/StringExt.kt
@@ -3,11 +3,5 @@ package solru.okkeipatcher.utils.extensions
 inline val String.Companion.empty: String
 	get() = ""
 
-fun String.trimIndents() = buildString {
-	this@trimIndents.lines().forEach {
-		append("${it.trimStart()}\n")
-	}
-}.removeSuffix("\n")
-
 fun String.isEmptyOrBlank() = this.isEmpty() || this.isBlank()
 fun String.isNotEmptyOrBlank() = this.isNotEmpty() && this.isNotBlank()
