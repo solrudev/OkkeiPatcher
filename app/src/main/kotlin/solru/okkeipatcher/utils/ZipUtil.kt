43925b9c64de072cc4249a0cc6065f27fc2f8b27
Ilya Fomichev <fomil.music@gmail.com>
2022-02-18 05:03:33 +0700

    cache regex

--- app/src/main/kotlin/solru/okkeipatcher/utils/ZipUtil.kt
+++ app/src/main/kotlin/solru/okkeipatcher/utils/ZipUtil.kt
@@ -2,10 +2,11 @@ package solru.okkeipatcher.utils
 
 import java.io.File
 
+private val tempZipFilesRegex = Regex("(apk|zip)\\d+")
+
 fun deleteTempZipFiles(directory: File?) {
 	if (directory == null || !directory.isDirectory) return
-	val regex = Regex("(apk|zip)\\d+")
 	directory.listFiles()
-		?.filter { it.extension.matches(regex) }
+		?.filter { it.extension.matches(tempZipFilesRegex) }
 		?.forEach { if (it.parentFile?.canWrite() == true) it.delete() }
 }
