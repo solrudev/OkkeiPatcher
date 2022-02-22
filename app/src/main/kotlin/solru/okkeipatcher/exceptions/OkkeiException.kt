130634264f42898f6fdc7ee23618bd7b3980f3f4
Ilya Fomichev <fomil.music@gmail.com>
2022-02-22 07:30:56 +0700

    delete OkkeiFatalException override message in OkkeiException to resolve on each access

--- app/src/main/kotlin/solru/okkeipatcher/exceptions/OkkeiException.kt
+++ app/src/main/kotlin/solru/okkeipatcher/exceptions/OkkeiException.kt
@@ -3,5 +3,8 @@ package solru.okkeipatcher.exceptions
 import solru.okkeipatcher.OkkeiApplication
 import solru.okkeipatcher.data.LocalizedString
 
-open class OkkeiException(val localizedMessage: LocalizedString, cause: Throwable? = null) :
-	Exception(localizedMessage.resolve(OkkeiApplication.context).toString(), cause)
+open class OkkeiException(val localizedMessage: LocalizedString, cause: Throwable? = null) : Exception(cause) {
+
+	override val message: String
+		get() = localizedMessage.resolve(OkkeiApplication.context).toString()
+}
--- app/src/main/kotlin/solru/okkeipatcher/exceptions/OkkeiFatalException.kt
+++
@@ -1,6 +0,0 @@
-package solru.okkeipatcher.exceptions
-
-import solru.okkeipatcher.data.LocalizedString
-
-class OkkeiFatalException(localizedMessage: LocalizedString, cause: Throwable?) :
-	OkkeiException(localizedMessage, cause)
