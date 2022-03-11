a99d6ca12867c36881edc7e2a3c69a90a49d8d0e
Ilya Fomichev <fomil.music@gmail.com>
2022-03-12 00:00:25 +0700

    reduce progress emits count

--- app/src/main/kotlin/solru/okkeipatcher/io/utils/ProgressUtil.kt
+++ app/src/main/kotlin/solru/okkeipatcher/io/utils/ProgressUtil.kt
@@ -2,6 +2,6 @@ package solru.okkeipatcher.io.utils
 
 import kotlin.math.ceil
 
-private const val EMIT_COUNT = 400
+private const val EMIT_COUNT = 100
 fun calculateProgressRatio(totalSize: Long, bufferLength: Long) =
 	ceil(totalSize.toDouble() / (bufferLength * EMIT_COUNT)).toInt().coerceAtLeast(1)
