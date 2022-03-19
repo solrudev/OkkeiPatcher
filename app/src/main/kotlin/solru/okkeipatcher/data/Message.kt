5f9a242772a4f49a046e6d32b0538d05e0248108
Ilya Fomichev <fomil.music@gmail.com>
2022-03-20 03:09:24 +0700

    remove message empty static property

--- app/src/main/kotlin/solru/okkeipatcher/data/Message.kt
+++ app/src/main/kotlin/solru/okkeipatcher/data/Message.kt
@@ -3,9 +3,4 @@ package solru.okkeipatcher.data
 data class Message(
 	val title: LocalizedString,
 	val message: LocalizedString
-) {
-
-	companion object {
-		val empty = Message(LocalizedString.empty(), LocalizedString.empty())
-	}
-}
+)
