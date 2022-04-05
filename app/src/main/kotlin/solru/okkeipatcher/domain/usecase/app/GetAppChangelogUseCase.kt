4e4868784d96009cdf56513bed408bad65b77f0c
Ilya Fomichev <fomil.music@gmail.com>
2022-04-06 03:06:35 +0700

    inject application context to get changelog use-case

--- app/src/main/kotlin/solru/okkeipatcher/domain/usecase/app/GetAppChangelogUseCase.kt
+++ app/src/main/kotlin/solru/okkeipatcher/domain/usecase/app/GetAppChangelogUseCase.kt
@@ -1,8 +1,7 @@
 package solru.okkeipatcher.domain.usecase.app
 
-import android.content.Context
 import solru.okkeipatcher.data.network.model.OkkeiPatcherChangelogDto
 
 interface GetAppChangelogUseCase {
-	suspend operator fun invoke(context: Context): OkkeiPatcherChangelogDto
+	suspend operator fun invoke(): OkkeiPatcherChangelogDto
 }
--- app/src/main/kotlin/solru/okkeipatcher/domain/usecase/app/impl/GetAppChangelogUseCaseImpl.kt
+++ app/src/main/kotlin/solru/okkeipatcher/domain/usecase/app/impl/GetAppChangelogUseCaseImpl.kt
@@ -2,16 +2,19 @@ package solru.okkeipatcher.domain.usecase.app.impl
 
 import android.content.Context
 import androidx.core.os.ConfigurationCompat
+import dagger.hilt.android.qualifiers.ApplicationContext
 import solru.okkeipatcher.data.network.model.OkkeiPatcherChangelogDto
 import solru.okkeipatcher.domain.repository.app.OkkeiPatcherRepository
 import solru.okkeipatcher.domain.usecase.app.GetAppChangelogUseCase
 import javax.inject.Inject
 
-class GetAppChangelogUseCaseImpl @Inject constructor(private val okkeiPatcherRepository: OkkeiPatcherRepository) :
-	GetAppChangelogUseCase {
+class GetAppChangelogUseCaseImpl @Inject constructor(
+	private val okkeiPatcherRepository: OkkeiPatcherRepository,
+	@ApplicationContext private val applicationContext: Context
+) : GetAppChangelogUseCase {
 
-	override suspend fun invoke(context: Context): OkkeiPatcherChangelogDto {
-		val locale = ConfigurationCompat.getLocales(context.resources.configuration)[0]
+	override suspend fun invoke(): OkkeiPatcherChangelogDto {
+		val locale = ConfigurationCompat.getLocales(applicationContext.resources.configuration)[0]
 		return okkeiPatcherRepository.getChangelog(locale)
 	}
 }
