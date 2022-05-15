e3f2020f304dbce8ee086ebf2846b5e0189b2fba
Ilya Fomichev <fomil.music@gmail.com>
2022-05-15 07:28:43 +0700

    remove unnecessary dependency from mock patch updates use-case

--- app/src/mock/kotlin/ru/solrudev/okkeipatcher/domain/usecase/patch/MockGetPatchUpdatesUseCase.kt
+++ app/src/mock/kotlin/ru/solrudev/okkeipatcher/domain/usecase/patch/MockGetPatchUpdatesUseCase.kt
@@ -1,11 +1,8 @@
 package ru.solrudev.okkeipatcher.domain.usecase.patch
 
 import ru.solrudev.okkeipatcher.domain.model.patchupdates.DefaultPatchUpdates
-import ru.solrudev.okkeipatcher.domain.repository.patch.DefaultPatchRepository
 import javax.inject.Inject
 
-class MockGetPatchUpdatesUseCase @Inject constructor(private val patchRepository: DefaultPatchRepository) :
-	GetPatchUpdatesUseCase {
-
+class MockGetPatchUpdatesUseCase @Inject constructor() : GetPatchUpdatesUseCase {
 	override suspend fun invoke() = DefaultPatchUpdates()
 }
