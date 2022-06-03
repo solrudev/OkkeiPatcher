750a325cdd42c8ad0727440e7f3d7442100fb6a4
Ilya Fomichev <fomil.music@gmail.com>
2022-06-04 04:26:12 +0700

    upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
-	val androidGradleVersion: String by extra("7.2.0")
+	val androidGradleVersion: String by extra("7.2.1")
 	val kotlinVersion: String by extra("1.6.21")
 	val hiltVersion: String by extra("2.42")
 	val navigationVersion: String by extra("2.4.2")
