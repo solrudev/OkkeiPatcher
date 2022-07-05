edccacddaf2f2514961afa1350ff79e9fee54a29
Ilya Fomichev <fomil.music@gmail.com>
2022-07-05 20:03:35 +0700

    move PatchFile to PatchRepository file

--- app/src/main/kotlin/ru/solrudev/okkeipatcher/domain/repository/patch/DefaultPatchRepository.kt
+++ app/src/main/kotlin/ru/solrudev/okkeipatcher/domain/repository/patch/DefaultPatchRepository.kt
@@ -1,14 +1,5 @@
 package ru.solrudev.okkeipatcher.domain.repository.patch
 
-import ru.solrudev.okkeipatcher.domain.core.persistence.Dao
-import ru.solrudev.okkeipatcher.domain.model.PatchFileData
-
-interface PatchFile {
-	val installedVersion: Dao<Int>
-	suspend fun getData(): PatchFileData
-	suspend fun isUpdateAvailable(): Boolean
-}
-
 interface DefaultPatchRepository : PatchRepository {
 	val scripts: PatchFile
 	val obb: PatchFile
--- app/src/main/kotlin/ru/solrudev/okkeipatcher/domain/repository/patch/PatchRepository.kt
+++ app/src/main/kotlin/ru/solrudev/okkeipatcher/domain/repository/patch/PatchRepository.kt
@@ -1,8 +1,16 @@
 package ru.solrudev.okkeipatcher.domain.repository.patch
 
+import ru.solrudev.okkeipatcher.domain.core.persistence.Dao
+import ru.solrudev.okkeipatcher.domain.model.PatchFileData
 import ru.solrudev.okkeipatcher.domain.model.patchupdates.PatchUpdates
 
 interface PatchRepository {
 	suspend fun getPatchUpdates(): PatchUpdates
 	suspend fun getPatchSizeInMb(): Double
+}
+
+interface PatchFile {
+	val installedVersion: Dao<Int>
+	suspend fun getData(): PatchFileData
+	suspend fun isUpdateAvailable(): Boolean
 }
