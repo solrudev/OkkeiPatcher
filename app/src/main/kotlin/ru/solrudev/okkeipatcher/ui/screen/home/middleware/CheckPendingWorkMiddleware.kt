25fdadf07f22b3a1d143357f05ac2a62327a66c7
Ilya Fomichev <fomil.music@gmail.com>
2022-07-18 23:46:13 +0700

    fix navigation issues when finished work is pending and permissions are not granted

--- app/src/main/kotlin/ru/solrudev/okkeipatcher/ui/screen/home/middleware/CheckPendingWorkMiddleware.kt
+++ app/src/main/kotlin/ru/solrudev/okkeipatcher/ui/screen/home/middleware/CheckPendingWorkMiddleware.kt
@@ -1,9 +1,11 @@
 package ru.solrudev.okkeipatcher.ui.screen.home.middleware
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import ru.solrudev.okkeipatcher.domain.usecase.work.CancelWorkUseCase
 import ru.solrudev.okkeipatcher.domain.usecase.work.GetPendingWorkUseCase
+import ru.solrudev.okkeipatcher.domain.usecase.work.GetWorkStateFlowUseCase
 import ru.solrudev.okkeipatcher.ui.core.Middleware
 import ru.solrudev.okkeipatcher.ui.core.collectEvent
 import ru.solrudev.okkeipatcher.ui.screen.home.model.HomeEvent
@@ -13,13 +15,15 @@ import javax.inject.Inject
 
 class CheckPendingWorkMiddleware @Inject constructor(
 	private val getPendingWorkUseCase: GetPendingWorkUseCase,
+	private val getWorkStateFlowUseCase: GetWorkStateFlowUseCase,
 	private val cancelWorkUseCase: CancelWorkUseCase
 ) : Middleware<HomeEvent> {
 
 	override fun apply(events: Flow<HomeEvent>) = flow {
 		events.collectEvent<PermissionsChecked> { event ->
 			val pendingWork = getPendingWorkUseCase() ?: return@collectEvent
-			if (event.allPermissionsGranted) {
+			val workState = getWorkStateFlowUseCase(pendingWork).first()
+			if (event.allPermissionsGranted || workState.isFinished) {
 				emit(WorkIsPending(pendingWork))
 			} else {
 				cancelWorkUseCase(pendingWork)
