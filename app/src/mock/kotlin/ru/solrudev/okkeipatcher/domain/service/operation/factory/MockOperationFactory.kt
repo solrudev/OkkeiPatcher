c18fffa7421d16cbb3d1b2f897189216316fc10c
Ilya Fomichev <fomil.music@gmail.com>
2022-08-12 02:39:52 +0700

    accept Persistable in MockOperationFactory constructor

--- app/src/mock/kotlin/ru/solrudev/okkeipatcher/domain/service/operation/factory/MockOperationFactory.kt
+++ app/src/mock/kotlin/ru/solrudev/okkeipatcher/domain/service/operation/factory/MockOperationFactory.kt
@@ -1,11 +1,11 @@
 package ru.solrudev.okkeipatcher.domain.service.operation.factory
 
 import ru.solrudev.okkeipatcher.domain.core.Result
-import ru.solrudev.okkeipatcher.domain.core.persistence.Dao
+import ru.solrudev.okkeipatcher.domain.core.persistence.Persistable
 import ru.solrudev.okkeipatcher.domain.service.operation.MockOperation
 
 class MockOperationFactory(
-	private val isPatchedDao: Dao<Boolean>,
+	private val isPatchedDao: Persistable<Boolean>,
 	private val tags: Set<String>
 ) : OperationFactory<Result> {
 
