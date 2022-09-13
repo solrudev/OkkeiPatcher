f2e176bd8aaab55498a22d78f0717d2f51b9c879
Ilya Fomichev <fomil.music@gmail.com>
2022-09-14 01:34:21 +0700

    update navigation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ buildscript {
 	val androidGradleVersion: String by extra("7.2.2")
 	val kotlinVersion: String by extra("1.7.10")
 	val hiltVersion: String by extra("2.43.2")
-	val navigationVersion: String by extra("2.5.1")
+	val navigationVersion: String by extra("2.5.2")
 
 	repositories {
 		google()
