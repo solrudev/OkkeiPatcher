2b761245ee661c46e3eb4220dfd5b69b26e7d475
Ilya Fomichev <fomil.music@gmail.com>
2022-10-05 01:56:07 +0700

    fix inject annotation

--- app/src/main/kotlin/ru/solrudev/okkeipatcher/data/worker/DownloadUpdateWorker.kt
+++ app/src/main/kotlin/ru/solrudev/okkeipatcher/data/worker/DownloadUpdateWorker.kt
@@ -5,16 +5,16 @@ import androidx.hilt.work.HiltWorker
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import ru.solrudev.okkeipatcher.R
 import ru.solrudev.okkeipatcher.data.service.NotificationService
 import ru.solrudev.okkeipatcher.domain.core.LocalizedString
 import ru.solrudev.okkeipatcher.domain.service.operation.factory.DownloadUpdateOperationFactory
-import javax.inject.Inject
 
 private val workLabel = LocalizedString.resource(R.string.notification_title_downloading_update)
 
 @HiltWorker
-class DownloadUpdateWorker @Inject constructor(
+class DownloadUpdateWorker @AssistedInject constructor(
 	@Assisted context: Context,
 	@Assisted workerParameters: WorkerParameters,
 	notificationService: NotificationService,
