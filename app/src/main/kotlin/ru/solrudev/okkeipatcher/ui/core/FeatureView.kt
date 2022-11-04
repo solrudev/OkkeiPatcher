c3bfa05856a6cff365e8a0674e12066ae2321e73
Ilya Fomichev <fomil.music@gmail.com>
2022-11-04 22:02:27 +0700

    make render() in FeatureView a default empty method

--- app/src/main/kotlin/ru/solrudev/okkeipatcher/ui/core/FeatureView.kt
+++ app/src/main/kotlin/ru/solrudev/okkeipatcher/ui/core/FeatureView.kt
@@ -4,5 +4,5 @@ package ru.solrudev.okkeipatcher.ui.core
  * A view which can render UI state object.
  */
 interface FeatureView<in S : UiState> {
-	fun render(uiState: S)
+	fun render(uiState: S) {}
 }
