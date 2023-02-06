f859e8e7ab3a61583e3b30ca7ff935ab07883b57
Ilya Fomichev <fomil.music@gmail.com>
2023-02-06 21:54:30 +0500

    fix imports

--- app/src/prod/kotlin/ru/solrudev/okkeipatcher/di/RepositoryFlavorModule.kt
+++ app/src/prod/kotlin/ru/solrudev/okkeipatcher/di/RepositoryFlavorModule.kt
@@ -6,12 +6,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ru.solrudev.okkeipatcher.app.repository.OkkeiPatcherRepository
+import ru.solrudev.okkeipatcher.app.repository.work.PatchWorkRepository
+import ru.solrudev.okkeipatcher.app.repository.work.RestoreWorkRepository
 import ru.solrudev.okkeipatcher.data.repository.app.OkkeiPatcherRepositoryImpl
-import ru.solrudev.okkeipatcher.data.repository.work.PatchWorkRepositoryImpl
-import ru.solrudev.okkeipatcher.data.repository.work.RestoreWorkRepositoryImpl
-import ru.solrudev.okkeipatcher.domain.repository.app.OkkeiPatcherRepository
-import ru.solrudev.okkeipatcher.domain.repository.work.PatchWorkRepository
-import ru.solrudev.okkeipatcher.domain.repository.work.RestoreWorkRepository
+import ru.solrudev.okkeipatcher.data.repository.app.work.PatchWorkRepositoryImpl
+import ru.solrudev.okkeipatcher.data.repository.app.work.RestoreWorkRepositoryImpl
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
