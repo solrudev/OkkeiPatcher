47ab6496a7e55a33b457d79ec5ba9ee884d71aaa
Ilya Fomichev <fomil.music@gmail.com>
2023-03-26 22:30:15 +0500

    rename use-case

--- app/src/main/kotlin/ru/solrudev/okkeipatcher/app/usecase/GetIsUpdateInstallPendingFlowUseCase.kt
+++ app/src/main/kotlin/ru/solrudev/okkeipatcher/app/usecase/GetIsUpdateInstallPendingFlowUseCase.kt
@@ -3,6 +3,6 @@ package ru.solrudev.okkeipatcher.app.usecase
 import ru.solrudev.okkeipatcher.app.repository.OkkeiPatcherRepository
 import javax.inject.Inject
 
-class GetIsUpdateInstallPendingFlow @Inject constructor(private val okkeiPatcherRepository: OkkeiPatcherRepository) {
+class GetIsUpdateInstallPendingFlowUseCase @Inject constructor(private val okkeiPatcherRepository: OkkeiPatcherRepository) {
 	operator fun invoke() = okkeiPatcherRepository.isUpdateInstallPending
 }
