2cf90f373246ab1b3ed98ede9d3c0d2cab3de803
Ilya Fomichev <fomil.music@gmail.com>
2023-03-28 01:06:56 +0500

    add broadcast receiver for ACTION_MY_PACKAGE_REPLACED to delete update file

---
+++ app/src/main/kotlin/ru/solrudev/okkeipatcher/data/repository/app/AppUpdateReceiver.kt
@@ -0,0 +1,29 @@
+package ru.solrudev.okkeipatcher.data.repository.app
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.ACTION_MY_PACKAGE_REPLACED
+import dagger.hilt.android.AndroidEntryPoint
+import okio.FileSystem
+import ru.solrudev.okkeipatcher.data.OkkeiEnvironment
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class AppUpdateReceiver : BroadcastReceiver() {
+
+	@Inject
+	lateinit var environment: OkkeiEnvironment
+
+	@Inject
+	lateinit var fileSystem: FileSystem
+
+	override fun onReceive(context: Context?, intent: Intent?) {
+		if (intent?.action != ACTION_MY_PACKAGE_REPLACED) {
+			return
+		}
+		val updateFile = environment.filesPath / APP_UPDATE_FILE_NAME
+		fileSystem.delete(updateFile)
+		println("AppUpdateReceiver: deleted update APK")
+	}
+}
