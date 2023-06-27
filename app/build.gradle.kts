616ebb892713a06f0191a1dfcd2ad63bb773c5eb
Ilya Fomichev <fomil.music@gmail.com>
2023-06-27 17:41:00 +0500

    increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
 		minSdk = 19
 		@SuppressLint("OldTargetApi")
 		targetSdk = 32
-		versionCode = 48
-		versionName = "2.0.6"
+		versionCode = 49
+		versionName = "2.0.7"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables.useSupportLibrary = true
 	}
