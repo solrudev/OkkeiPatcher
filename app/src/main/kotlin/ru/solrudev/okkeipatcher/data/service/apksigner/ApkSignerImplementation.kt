c4b2b7da45f2a3ac2aa1f6e8822476542ff58fd0
Ilya Fomichev <fomil.music@gmail.com>
2024-09-10 11:32:03 +0500

    update copyright year

--- app/src/main/kotlin/ru/solrudev/okkeipatcher/data/service/apksigner/ApkSignerImplementation.kt
+++ app/src/main/kotlin/ru/solrudev/okkeipatcher/data/service/apksigner/ApkSignerImplementation.kt
@@ -1,6 +1,6 @@
 /*
  * Okkei Patcher
- * Copyright (C) 2023 Ilya Fomichev
+ * Copyright (C) 2023-2024 Ilya Fomichev
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
