32bb1be349c1ed92df0fc566252cb6092126f3c4
Ilya Fomichev <fomil.music@gmail.com>
2025-01-11 20:08:58 +0500

    increment version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 		applicationId = packageName
 		minSdk = 19
 		targetSdk = 35
-		versionCode = 73
+		versionCode = 74
 		versionName = "2.1.2"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables.useSupportLibrary = true
